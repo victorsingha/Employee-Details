c6502d01a1cd5b8ba952baa67e5e3671a5bb61cf
victor-singha <vicunite2@gmail.com>
2021-06-10 15:43:08 +0530

    read data from form

--- js/adduser.js
+++ js/adduser.js
@@ -1,46 +1,53 @@
-let salary;
-let profilePicURL;
-let gender;
+var employeeData = {
+  name: "",
+  profile: "",
+  gender: "",
+  department: [],
+  salary: "",
+  startdate: "",
+  notes: "",
+};
 
 function updateRange(val) {
   document.getElementById("rangeValue").value = val;
   console.log(val);
-  salary = val;
+  employeeData.salary = val;
 }
 
 function getName() {
   const name = document.getElementById("name").value;
   console.log(name);
+  employeeData.name = name;
 }
 function getimage1() {
   const profile = document.getElementById("image1").value;
   console.log(profile);
-  profilePicURL = profile;
+  employeeData.profile = profile;
 }
 function getimage2() {
   const profile = document.getElementById("image2").value;
   console.log(profile);
-  profilePicURL = profile;
+  employeeData.profile = profile;
 }
 function getimage3() {
   const profile = document.getElementById("image3").value;
   console.log(profile);
-  profilePicURL = profile;
+  employeeData.profile = profile;
 }
 function getimage4() {
   const profile = document.getElementById("image4").value;
   console.log(profile);
-  profilePicURL = profile;
+  employeeData.profile = profile;
 }
 function getMale() {
   const gen = document.getElementById("male").value;
   console.log(gen);
-  gender = gen;
+  employeeData.gender = gen;
 }
 function getFemale() {
   const gen = document.getElementById("female").value;
   console.log(gen);
-  gender = gen;
+  employeeData.gender = gen;
 }
 
 function getCheckboxValue() {
@@ -51,5 +58,31 @@ function getCheckboxValue() {
       checkedValue.push(inputElements[i].value);
     }
   }
-  console.log(checkedValue);
+  // console.log(checkedValue);
+  return checkedValue;
 }
+function getStartDate() {
+  var day = document.getElementById("select-day").value;
+  var month = document.getElementById("select-month").value;
+  var year = document.getElementById("select-year").value;
+
+  var date = `${day}-${month}-${year}`;
+  return date;
+}
+function getNotes() {
+  const val = document.getElementById("notes").value;
+  console.log(val);
+  employeeData.notes = val;
+}
+document.querySelector("#submit").addEventListener("click", function (event) {
+  event.preventDefault();
+  employeeData.department = getCheckboxValue();
+  employeeData.startdate = getStartDate();
+  console.log(employeeData);
+});
+document.querySelector("#cancel").addEventListener("click", function (event) {
+  event.preventDefault();
+});
+document.querySelector("#reset").addEventListener("click", function (event) {
+  event.preventDefault();
+});
